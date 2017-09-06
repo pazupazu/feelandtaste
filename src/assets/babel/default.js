f134c758e34722880967cff1dd4c16d5bd691cdb
河西 <kkouhei3@gmail.com>
2017-09-07 01:28:03 +0900

    window resize timing only width

--- src/assets/babel/default.js
+++ src/assets/babel/default.js
@@ -44,10 +44,26 @@ $(document).ready(function() {
   // https://www.webcreatorbox.com/tech/object-fit
   var fullImage = document.querySelectorAll('img.full-image');
   objectFitImages(fullImage);
+
+  // スクロールクラス付加
+  var $secondSection = $('.section--2'),
+  secondSectionHeight = $secondSection.height();
+  $(window).on('load scroll', function() {
+    $('.btn-burger').addClass('btn-burger--top');
+    var scrollTop = $(this).scrollTop();
+    if ( scrollTop < secondSectionHeight ) {
+      $('.btn-burger').removeClass('btn-burger--top');
+    }
+    return false;
+  });
 });
 
 // fullpage.js
 $(document).ready(function() {
+  createFullpage();
+});
+
+function createFullpage() {
   $('#fullpage').fullpage({
     anchors:[
       'section1',
@@ -75,31 +91,64 @@ $(document).ready(function() {
       }
     }
   });
-});
-
+}
 // safari ランドスケープ リサイズ
-var height = window.innerHeight;
-
-$(window).on('load resize', function () {
+$(window).on('load', function () {
   responsive();
+  var h = window.innerHeight;
+  $('.index .section--top').css('height', h + 'px');
 });
 
+var currentWidth = window.innerWidth;
+$(window).on('resize', function () {
+  if (window.innerWidth != currentWidth ) {
+    currentWidth = window.innerWidth;
+    responsive();
+  } else {
+    return;
+  }
+});
+
+function device() {
+  var ua = navigator.userAgent;
+  if(ua.indexOf('iPad') > 0){return 'iPad'};
+}
+
+var height = window.innerHeight;
 function responsive() {
-  var w = $(window).width();
-  if ( w <= 768) {
+  // sectionTopに100vhではアイコンが出ない
+  var w = window.innerWidth;
+  var h = window.innerHeight;
+  $('.index .section--top').css('height', h + 'px');
+
+  if ( w <= 768 ) {
     $.fn.fullpage.destroy('all');
-    
     // destroyするとページ内リンクができない data-anchorタグが挿入されない
     $('.index .section').each(function(index){
       let i = index +1;
       $(this).attr('id', 'section' + i);
     });
+    // $('.index .section').css('height', 'auto');
   } else {
-    $('.index .section').css('height', height + 'px');
-    if(window.innerHeight != height) {
-      height = window.innerHeight;
-      $('.index .section').css('height', height + 'px');
-    }
+
+    $('.index .section').each(function(index){
+      let i = index +1;
+      $(this).removeAttr('id', 'section' + i);
+    });
+    // createFullpage();
+    // $('.index .section').css('height', height + 'px');
+    // if(window.innerHeight != height) {
+    //   height = window.innerHeight;
+    //   $('.index .section').css('height', height + 'px');
+    // }
+    // createFullpage();
+    // $('.index .section').css('height', h + 'px');
+    // var ch = window.innerHeight;
+    // var wh = $(window).height;
+    // if(window.innerHeight != ch) {
+    //   $('.index .section').css('height', ch + 'px');
+    //   ch = window.innerHeight;
+    // }
   }
 }
 
